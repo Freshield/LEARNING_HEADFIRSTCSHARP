57da392b666439f51397cd1cf5d154057f054ff6
Freshield <zxdsw199182@gmail.com>
2016-06-01 19:54:18 -0400

    done test1

---
+++ Chp3/Test1/Test1/Talker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Test1
+{
+    class Talker
+    {
+        public static int BlahBlahBlah(string thingToSay, int numberOfTimes)
+        {
+            string finalString = "";
+            for (int count = 1; count <= numberOfTimes; count++)
+            {
+                finalString += thingToSay + "\n";
+            }
+
+            MessageBox.Show(finalString);
+            return finalString.Length;
+        }
+    }
+}
