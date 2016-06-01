cad9e26962eb97b77db5d8c2805327c65c8ee91e
Freshield <zxdsw199182@gmail.com>
2016-05-31 21:15:36 -0400

    done today

--- Chp2/Form1/Form1/Form1.Designer.cs
+++ Chp2/Form1/Form1/Form1.Designer.cs
@@ -28,20 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.buttonnnn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // buttonnnn
+            // 
+            this.buttonnnn.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.buttonnnn.Location = new System.Drawing.Point(0, 0);
+            this.buttonnnn.Name = "buttonnnn";
+            this.buttonnnn.Size = new System.Drawing.Size(274, 201);
+            this.buttonnnn.TabIndex = 0;
+            this.buttonnnn.Text = "push";
+            this.buttonnnn.UseVisualStyleBackColor = false;
+            this.buttonnnn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(360, 54);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(538, 585);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(782, 510);
+            this.ClientSize = new System.Drawing.Size(983, 704);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonnnn);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Paper Company";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonnnn;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Chp2/Form1/Form1/Form1.cs
+++ Chp2/Form1/Form1/Form1.cs
@@ -15,5 +15,10 @@ namespace Form1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
     }
 }
