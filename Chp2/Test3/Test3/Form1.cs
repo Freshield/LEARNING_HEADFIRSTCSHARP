751bf0b4541a914e2ec1a50dc3b7d03900c6eb14
Freshield <zxdsw199182@gmail.com>
2016-06-01 17:07:23 -0400

    test box

---
+++ Chp2/Test3/Test3/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Test3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                if (label1.BackColor == Color.White)
+                {
+                    label1.BackColor = Color.Red;
+                }
+                else
+                {
+                    label1.BackColor = Color.White;
+                }
+            }
+            else
+            {
+                MessageBox.Show("The box is not checked");
+            }
+        }
+    }
+}
