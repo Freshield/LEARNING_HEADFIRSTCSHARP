f61828824df3d8c22eb88a92d2d0b7f60befa58f
Freshield <zxdsw199182@gmail.com>
2016-06-02 21:57:29 -0400

    done elephant

---
+++ Chp4/Test3/Test3/Elephant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Test3
+{
+    class Elephant
+    {
+        public String Name;
+        public int EarSize;
+
+        public void WhoAmI()
+        {
+            MessageBox.Show("My ears are " + EarSize + " inches tall.",Name + " says...");
+        }
+    }
+}
