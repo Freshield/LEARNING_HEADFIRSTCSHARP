05345802a3f5c4f8ee37c62df9c7660d876c1cea
Freshield <zxdsw199182@gmail.com>
2016-06-02 21:29:05 -0400

    done calculator

---
+++ Chp4/Test2/Test2/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Test2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.startMile = new System.Windows.Forms.NumericUpDown();
+            this.endMile = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.startMile)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.endMile)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(351, 48);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Starting Mileage";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(37, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(326, 48);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ending Mileage";
+            // 
+            // startMile
+            // 
+            this.startMile.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startMile.Location = new System.Drawing.Point(449, 49);
+            this.startMile.Maximum = new decimal(new int[] {
+            99999,
+            0,
+            0,
+            0});
+            this.startMile.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.startMile.Name = "startMile";
+            this.startMile.Size = new System.Drawing.Size(387, 56);
+            this.startMile.TabIndex = 2;
+            this.startMile.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // endMile
+            // 
+            this.endMile.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.endMile.Location = new System.Drawing.Point(449, 143);
+            this.endMile.Maximum = new decimal(new int[] {
+            99999,
+            0,
+            0,
+            0});
+            this.endMile.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.endMile.Name = "endMile";
+            this.endMile.Size = new System.Drawing.Size(387, 56);
+            this.endMile.TabIndex = 3;
+            this.endMile.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(60, 258);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(303, 48);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Amount Owed";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(441, 258);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 48);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "$";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(449, 384);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(238, 73);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(995, 537);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.endMile);
+            this.Controls.Add(this.startMile);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Mileage Calculator";
+            ((System.ComponentModel.ISupportInitialize)(this.startMile)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.endMile)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown startMile;
+        private System.Windows.Forms.NumericUpDown endMile;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Chp4/Test2/Test2/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Test2
+{
+    public partial class Form1 : Form
+    {
+        double milesTraveled;
+        double reimburseRate = 0.39;
+        double amountOwed;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (startMile.Value >= endMile.Value)
+            {
+                MessageBox.Show("The atarting mileage must be less than th ending mileage","Cannot Calculate");
+            }
+            else
+            {
+                milesTraveled = (double)(endMile.Value - startMile.Value);
+                amountOwed = milesTraveled * reimburseRate;
+                label4.Text = "$" + amountOwed;
+            }
+        }
+
+    }
+}
