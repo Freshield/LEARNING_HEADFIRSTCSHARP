27b7b563a5c5399d6c744b2436ad54fa55fc2489
Freshield <zxdsw199182@gmail.com>
2016-06-02 22:49:02 -0400

    done menu

---
+++ Chp4/Test4/Test4/MenuMaker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Test4
+{
+    class MenuMaker
+    {
+        public Random Randomizer;
+
+        string[] Meats = { "beef", "salami", "turkey", "ham", "pastrami" };
+        string[] Condiments = { "mustard", "brown", "honey", "mayo", "relish", "dressing" };
+        string[] Breads = { "rye", "white", "wheat", "pumpernickel", "italian", "rool" };
+
+        public string GetMenuItem()
+        {
+            string randomMeat = Meats[Randomizer.Next(Meats.Length)];
+            string randomCondiment = Condiments[Randomizer.Next(Condiments.Length)];
+            string randomBread = Breads[Randomizer.Next(Breads.Length)];
+
+            return randomMeat + " with " + randomCondiment + " on " + randomBread;
+        }
+    }
+}
