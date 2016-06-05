066459d398f33dd9f82d2fe6c022c607a4c7bdce
Freshield <zxdsw199182@gmail.com>
2016-06-04 20:29:56 -0400

    done jewel steal

---
+++ Chp6/Chp6_Test2/Chp6_Test2/Jewels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chp6_Test2
+{
+    class Jewels
+    {
+        public string Sparkle()
+        {
+            return "Sparkle, sparkle!";
+        }
+    }
+}
---
+++ Chp6/Chp6_Test3/Chp6_Test3/Jewels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chp6_Test3
+{
+    class Jewels
+    {
+        public string Sparkle()
+        {
+            return "Sparkle, sparkle!";
+        }
+    }
+}
