5d0558707ad5191c991ac6df2d0b7765e083097b
Freshield <zxdsw199182@gmail.com>
2016-06-05 18:51:36 -0400

    done Chp7 Test1

---
+++ Chp7/Chp7_Test1/Chp7_Test1/Bee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chp7_Test1
+{
+    class Bee : IStingPatrol
+    {
+        public int AlertLevel { get; set; }
+
+        public int StingerLength { get; set; }
+
+        public bool LookForEnemies()
+        {
+            return false;
+        }
+
+        public int SharpenStinger(int length)
+        {
+            return length;
+        }
+
+    }
+}
---
+++ Chp7/Chp7_Test1/Chp7_Test1/IStingPatrol.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chp7_Test1
+{
+    interface IStingPatrol
+    {
+        int AlertLevel { get; }
+        int StingerLength { get; set; }
+        bool LookForEnemies();
+        int SharpenStinger(int length);
+    }
+}
