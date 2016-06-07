ef08bf84abd068618de564b7e2ab5407113d4aba
Freshield <zxdsw199182@gmail.com>
2016-06-06 21:21:07 -0400

    done test3

---
+++ Chp8/Chp8_Test3/Chp8_Test3/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Chp8_Test3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            List<Shoe> shoeCloset = new List<Shoe>();
+
+            shoeCloset.Add(new Shoe() { Style = Style.sneakers, Color = "black"});
+            shoeCloset.Add(new Shoe() { Style = Style.clogs, Color = "brown" });
+            shoeCloset.Add(new Shoe() { Style = Style.wingtips, Color = "black" });
+            shoeCloset.Add(new Shoe() { Style = Style.loafers, Color = "whilte" });
+            shoeCloset.Add(new Shoe() { Style = Style.sneakers, Color = "green" });
+
+            int numberOfShoes = shoeCloset.Count;
+            foreach (Shoe shoe in shoeCloset)
+            {
+                shoe.Style = Style.flipflops;
+                shoe.Color = "orange";
+            }
+
+            shoeCloset.RemoveAt(4);
+
+            Shoe thirdShoe = shoeCloset[3];
+            Shoe secondShoe = shoeCloset[2];
+            shoeCloset.Clear();
+
+            shoeCloset.Add(thirdShoe);
+            if (shoeCloset.Contains(secondShoe))
+            {
+                MessageBox.Show("That's surprising.");
+            }
+        }
+    }
+}
