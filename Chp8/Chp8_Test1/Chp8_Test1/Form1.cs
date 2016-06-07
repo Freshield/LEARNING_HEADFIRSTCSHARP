4fb0096894183e775142737cd583e41cfb46e2c6
Freshield <zxdsw199182@gmail.com>
2016-06-06 20:07:58 -0400

    done Test1

---
+++ Chp8/Chp8_Test1/Chp8_Test1/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Chp8_Test1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public enum TrickScore
+        {
+            sit = 7,
+            beg = 25,
+            rollover = 50,
+            fetch = 10,
+            comehere = 5,
+            speak = 30
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int value = (int)TrickScore.fetch * 3;
+            TrickScore score = (TrickScore)value;
+            MessageBox.Show(value.ToString() + score.ToString());
+        }
+    }
+}
