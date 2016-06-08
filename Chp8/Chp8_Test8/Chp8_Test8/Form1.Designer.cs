d3551a3552d627aba73a546a2ab039ec4aa9ba46
Freshield <zxdsw199182@gmail.com>
2016-06-07 22:38:54 -0400

    done deck

--- Chp8/Chp8_Test8/Chp8_Test8/Form1.Designer.cs
+++ Chp8/Chp8_Test8/Chp8_Test8/Form1.Designer.cs
@@ -28,34 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.reset1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.reset2 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // reset1
             // 
-            this.button1.Location = new System.Drawing.Point(49, 556);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(183, 83);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.reset1.Location = new System.Drawing.Point(48, 590);
+            this.reset1.Name = "reset1";
+            this.reset1.Size = new System.Drawing.Size(198, 83);
+            this.reset1.TabIndex = 0;
+            this.reset1.Text = "reset1";
+            this.reset1.UseVisualStyleBackColor = true;
+            this.reset1.Click += new System.EventHandler(this.reset1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 31;
+            this.listBox1.Location = new System.Drawing.Point(29, 63);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(253, 500);
+            this.listBox1.TabIndex = 1;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 31;
+            this.listBox2.Location = new System.Drawing.Point(459, 63);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(268, 500);
+            this.listBox2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(314, 165);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 63);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "<";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(314, 269);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 65);
+            this.button3.TabIndex = 4;
+            this.button3.Text = ">";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // reset2
+            // 
+            this.reset2.Location = new System.Drawing.Point(484, 592);
+            this.reset2.Name = "reset2";
+            this.reset2.Size = new System.Drawing.Size(198, 81);
+            this.reset2.TabIndex = 5;
+            this.reset2.Text = "reset2";
+            this.reset2.UseVisualStyleBackColor = true;
+            this.reset2.Click += new System.EventHandler(this.reset2_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(48, 689);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(198, 81);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(484, 700);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(198, 81);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 32);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(453, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 32);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "label2";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(732, 748);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(752, 842);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.reset2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.reset1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button reset1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button reset2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Chp8/Chp8_Test8/Chp8_Test8/Form1.cs
+++ Chp8/Chp8_Test8/Chp8_Test8/Form1.cs
@@ -12,18 +12,105 @@ namespace Chp8_Test8
     public partial class Form1 : Form
     {
         Card cardToCheck;
-        
+        Deck deck1;
+        Deck deck2;
+        Random random = new Random();
 
         public Form1()
         {
             InitializeComponent();
+            ResetDeck(1);
+            ResetDeck(2);
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void RedrawDeck(int DeckNumber)
         {
-            cardToCheck = new Card(3, Kind.Ace);
-            bool doesItMatch = Card.DoesCardMatch(cardToCheck, Kind.Four);
-            MessageBox.Show(doesItMatch.ToString());
+            if (DeckNumber == 1)
+            {
+                listBox1.Items.Clear();
+                foreach (string cardName in deck1.GetCardNames())
+                {
+                    listBox1.Items.Add(cardName);
+                }
+                label1.Text = "Deck #1 (" + deck1.Count + " cards)";
+            }
+            else
+            {
+                listBox2.Items.Clear();
+                foreach (string cardName in deck2.GetCardNames())
+                {
+                    listBox2.Items.Add(cardName);
+                }
+                label2.Text = "Deck #2 (" + deck2.Count + " cards)";
+            }
+        }
+
+        private void ResetDeck(int deckNumber)
+        {
+            if (deckNumber == 1)
+            {
+                int numberOfCards = random.Next(1, 11);
+                deck1 = new Deck(new Card[] { });
+                for (int i = 0; i < numberOfCards; i++)
+                {
+                    deck1.Add(new Card(random.Next(1,14),(Kind)random.Next(1,4)));
+                }
+                deck1.Sort();
+            }
+            else
+            {
+                deck2 = new Deck();
+            }
+        }
+
+        private void reset1_Click(object sender, EventArgs e)
+        {
+            ResetDeck(1);
+            RedrawDeck(1);
+        }
+
+        private void reset2_Click(object sender, EventArgs e)
+        {
+            ResetDeck(2);
+            RedrawDeck(2);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            deck1.Shuffle();
+            RedrawDeck(1);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            deck2.Shuffle();
+            RedrawDeck(2);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox2.SelectedIndex >= 0)
+            {
+                if (deck2.Count > 0)
+                {
+                    deck1.Add(deck2.Deal(listBox2.SelectedIndex));
+                }
+            }
+            RedrawDeck(1);
+            RedrawDeck(2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex >= 0)
+            {
+                if (deck1.Count > 0)
+                {
+                    deck2.Add(deck1.Deal(listBox1.SelectedIndex));
+                }
+            }
+            RedrawDeck(1);
+            RedrawDeck(2);
         }
     }
 }
