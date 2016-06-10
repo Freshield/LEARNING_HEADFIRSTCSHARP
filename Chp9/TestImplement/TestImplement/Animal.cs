19e6b3c03fd356ea4c85fc5b4de582862465a7f2
Freshield <zxdsw199182@gmail.com>
2016-06-10 18:54:51 -0400

    done implement test

---
+++ Chp9/TestImplement/TestImplement/Animal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestImplement
+{
+    class Animal
+    {
+        public int age;
+        public void eat()
+        {
+            Console.WriteLine("Animal eat");
+        }
+    }
+}
---
+++ Chp9/TestImplement/TestImplement/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TestImplement
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Animal animal = new Human();
+            if (animal is Human)
+            {
+                Human human = animal as Human;
+                human.work();
+
+
+            }
+        }
+    }
+}
