385b485936ce594fc47183e9378055a17ce13101
Freshield <zxdsw199182@gmail.com>
2016-06-09 20:04:47 -0400

    done test ineritance

---
+++ Chp8/TestIneritance/TestIneritance/Animal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestIneritance
+{
+    class Animal
+    {
+        public virtual void eat()
+        {
+            Console.WriteLine("Animal eat");
+        }
+    }
+}
---
+++ Chp8/TestIneritance/TestIneritance/Human.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestIneritance
+{
+    class Human : Animal
+    {
+        public override void eat()
+        {
+            Console.WriteLine("Human eat");
+            base.eat();
+        }
+    }
+}
