52e747c7cb465af65cba8d3b71203889959bdb27
Freshield <zxdsw199182@gmail.com>
2016-06-12 15:49:25 -0400

    chp10 begin

---
+++ Chp10/Chp10_Test/Chp10_Test/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Chp10_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Console.WriteLine("1");
+                int[] a = new int[3];
+                a[5] = 10;
+                Console.WriteLine("1.5");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("2");
+            }
+            Console.WriteLine("3");
+        }
+    }
+}
