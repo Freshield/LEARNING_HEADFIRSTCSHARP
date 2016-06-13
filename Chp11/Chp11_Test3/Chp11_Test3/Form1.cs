c6d1dc643dda986f6ff80f245626bbd6e26d165a
Freshield <zxdsw199182@gmail.com>
2016-06-13 16:51:58 -0400

    test delegate

---
+++ Chp11/Chp11_Test3/Chp11_Test3/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Chp11_Test3
+{
+    public partial class Form1 : Form
+    {
+        ConvertsIntToString someMethod = new ConvertsIntToString(HiThere);
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static string HiThere(int i)
+        {
+            return "Hi there! #" + (i * 100);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string message = someMethod(5);
+            Console.WriteLine(message);
+        }
+    }
+}
