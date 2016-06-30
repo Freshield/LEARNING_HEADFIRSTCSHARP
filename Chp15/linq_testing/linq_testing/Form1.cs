ed4993fb11b80b8a30864780b5ac24562d94f7fb
Freshield <zxdsw199182@gmail.com>
2016-06-29 21:58:19 -0400

    fix the problem

--- Chp15/linq_testing/linq_testing/Form1.cs
+++ Chp15/linq_testing/linq_testing/Form1.cs
@@ -18,16 +18,27 @@ namespace linq_testing
 
         private void button1_Click(object sender, EventArgs e)
         {
+
+            
             int[] values = new int[] { 0, 12, 44, 36, 92, 54, 13, 8 };
             var result = from v in values
                          where v < 37
                          orderby v
-                         select v;
+                         select v + 1;
             foreach (int i in result)
             {
                 Console.WriteLine("{0} ", i);
 
             }
+
+            string[] words = new string[] { "a", "b", "c" };
+            var results = from word in words
+                          select word + " 233 ";
+            foreach (var i in results)
+            {
+                Console.WriteLine(i);
+            }
+             
         }
     }
 }
