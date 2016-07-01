1de4df130148f95026dc0283646d21fbe8d53aae
Freshield <zxdsw199182@gmail.com>
2016-07-01 19:08:18 -0400

    done extension

---
+++ GC/MyExtensions/MyExtensions/HumanEtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyExtensions
+{
+    static class HumanEtensions
+    {
+        public static bool IsDistressCall(this string s)
+        {
+            if (s.Contains("Help"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
