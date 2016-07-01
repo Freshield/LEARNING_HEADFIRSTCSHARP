3be6fef311a8bed300c12362ef2b9dd8e41a098a
Freshield <zxdsw199182@gmail.com>
2016-06-30 21:31:09 -0400

    test linq

---
+++ Chp15/linq_testing/linq_testing/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace linq_testing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] values = new int[] { 0, 12, 44, 36, 92, 54, 13, 8 };
+            var result = from v in values
+                         where v < 37
+                         orderby v
+                         select v;
+            foreach (int i in result)
+            {
+                Console.WriteLine("{0} ", i);
+
+            }
+        }
+    }
+}
